b909982b43d8fe442fe59435bd78e89b42fc0858
jeanfe <maestratijf@gmail.com>
2024-02-22 22:15:48 +0100

    patron quasi fini, reste a tout implémenter puis tester

--- Learning/gsplat_rust_candle/src/cuda/customop.rs
+++ Learning/gsplat_rust_candle/src/cuda/customop.rs
@@ -28,23 +28,49 @@ pub fn ProjectGaussians(num_points : int,
             arg.unsqueeze(0);
         }
     }
-    let tensor_in = stack(&A,0);
+    let tensor_in = stack(&A,1);
     let c = ProjectGaussians{glob_scale,fx,fy,cx,cy,img_height,img_width,tile_bounds,clip_thresh,viewmat,projmat};
     
     //reecriture de la fonction apply_op1_arc de tensor.rs pour bypass le fonctionnement normal
     let (storage1, shape1, storage2, shape2, storage3, shape3, storage4, shape4, storage5, shape5, storage6, shape6, storage7, shape7) = c.fwd((means3d.storage())?,(scales.storage())?,(quats.storage())?,(viewmat.storage())?,(projmat.storage())?);
 
     //A FAIRE : Créer storage à partir d'un tenseur qui est la concatenation de tenseur initialisé avec les storages et shapes
+    tensor1 = from_storage(storage1, shape1, Option::None, false);
+    tensor2 = from_storage(storage2, shape2, Option::None, false);
+    tensor3 = from_storage(storage3, shape3, Option::None, false);
+    tensor4 = from_storage(storage4, shape4, Option::None, false);
+    tensor5 = from_storage(storage5, shape5, Option::None, false);
+    tensor6 = from_storage(storage6, shape6, Option::None, false);
+    tensor7 = from_storage(storage7, shape7, Option::None, false);
 
+    tensortot = cat(&[tensor1,tensor2,tensor3,tensor4,tensor5,tensor6,tensor7],1);
+
+    //réécriture de from_storage et copy
+    shape = tensortot.shape();
+    let storage = tensortot.storage().try_clone(self.layout())?;
+    let dtype = storage.dtype();
+    let device = storage.device();
     let op = BackpropOp::new1(tensor_in, |s| Op::CustomOp1(s, c.clone()));
-    let tensor_out = Ok(from_storage(storage, shape, op, false));
+    let tensor_out = Tensor::Tensor_ {
+        id: TensorId::new(),
+        storage: Arc::new(RwLock::new(storage)),
+        layout: Layout::contiguous(shape),
+        Option::None,
+        op,
+        dtype,
+        device,
+    }
 
     //A FAIRE : Decomposer tensor_out en les sortie
     //la backpropagation va marcher puisque chunk utilise narrow qui va associer l'opération Backprop narrow aux tenseurs de sorte à ce que les gradients des tenseurs
     //de sortie reforme un tenseur unique de ces gradient, qui pourra etre rentré dans le backward de ProjectGaussian (le struct) qu'on re splitera pour donner au kernel cuda
     //et les gradients de seront remis dans 1 gradient dans bckward de ProjectGaussian(le struct) qui sera ensuite re-split dans les bons tenseur par la backpropagation grace à l'opération Cat
-    chunks = ...;
-    dims = ...;
-    let decomp = tensor_out.chunk(chunks,dims).expect("Problème de chunking de tensor_out");
+    
+    //SURTOUT : RENOMMER LES "decomp1", "tensor1" etc... avec les noms des variables représentée
+    //Faire un truc comme ça avec les narrow, ça marchera bien pour la backprop
+    let decomp1 = tensor_out.narrow(1,0,6);
+    let decomp2 = tensor_out.narrow(1,6,2);
+    
+    
     Ok((decomp[0],decomp[1],decomp[2],decomp[3],decomp[4],decomp[5],decomp[6]))
 }
